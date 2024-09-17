e36e27ea566bb7a2c3ea920632c45143f24edb7b
Huskarnov <youcef.merad.batna2m@gmail.com>
2024-09-17 23:19:07 +0100

    d

--- javascript.js
+++ javascript.js
@@ -1,72 +1,63 @@
 let humanScore = 0;
 let PCScore =0;
-let rounds = 0;
+let scoreH = document.querySelector('.score-h');
+let scoreC = document.querySelector('score-c');
 
-function PlayGame(){
-    rounds++;
-    if(rounds < 6){
-    let humanChoice = (prompt("Chose between rock, paper, scisors!")).toLocaleLowerCase();
+let insideWindow = document.querySelector('.inside-game-window');
 
+let newGameButton = document.querySelector('.new-game');
+let rockButton = document.querySelector('.rock');
+let paperButton = document.querySelector('.paper');
+let scisorsButton = document.querySelector('.scisors');
+
+ 
+
+
+newGameButton.addEventListener("click", function(){
+    newGameButton.setAttribute("style", "display:none");
+    insideWindow.removeAttribute("id");
+    insideWindow.classList.add("inside-game-window");
+
+
+});
+
+rockButton.addEventListener("click", PlayGame("rock"));
+paperButton.addEventListener("click", PlayGame("paper"));
+scisorsButton.addEventListener("click", PlayGame("scisors"));
+
+
+function PlayGame(choice){
     
-  
-   
+    if(!PCScore === 5 || !humanScore === 5){
+    let humanChoice = choice; 
+
     if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scisors" ){
 
         let rdm =Math.random() * 100;
         let pcChoice;
-        let choice =  rdm < 33.33?                 pcChoice = "rock":
-                      rdm > 33.33 && rdm < 66.66 ? pcChoice = "paper" :
-                                                   pcChoice = "scisors";
+        let choice =    rdm < 33.33?                    pcChoice = "rock":
+                        rdm > 33.33 && rdm < 66.66 ?    pcChoice = "paper" :
+                                                        pcChoice = "scisors";
 
                     if(humanChoice === pcChoice){
-                        alert(`Round: ${rounds} \n Tie, Human score: ${humanScore} PC score: ${PCScore}`);
-                        
-
                     }else if(humanChoice === "rock" && pcChoice === "paper"){
-                       
                         PCScore++;
-                        alert(`Round: ${rounds} \n ${humanChoice} vs ${pcChoice} PC wins \n Human score: ${humanScore} PC score: ${PCScore}`);
                     }else if (humanChoice === "rock" && pcChoice === "scisors"){
-                        
                         humanScore++;
-                        alert(`Round: ${rounds} \n  ${humanChoice} vs ${pcChoice} Human wins \n Human score: ${humanScore} PC score: ${PCScore}`);
                     }else if ((humanChoice === "paper" && pcChoice === "scisors")){
-                       
                         PCScore++;
-                        alert(`Round: ${rounds} \n ${humanChoice} vs ${pcChoice} PC wins \n Human score: ${humanScore} PC score: ${PCScore}`);
                     }else if ((humanChoice === "paper" && pcChoice === "rock")){
-                        
                         humanScore++;
-                        alert(`Round: ${rounds} \n ${humanChoice} vs ${pcChoice} Human wins \n Human score: ${humanScore} PC score: ${PCScore}`);
                     }else if ((humanChoice === "scisors" && pcChoice === "rock")){
-                        
                         PCScore++;
-                        alert(`Round: ${rounds} \n ${humanChoice} vs ${pcChoice} PC wins \n Human score: ${humanScore} PC score: ${PCScore}`);
                     }else if ((humanChoice === "scisors" && pcChoice === "paper")){
-                        
                         humanScore++;
-                        alert(`Round: ${rounds} \n ${humanChoice} vs ${pcChoice} Human wins \n Human score: ${humanScore} PC score: ${PCScore}`);
-                        
                     }
-                    PlayGame();
-
-    }else{
-        alert("worng choice, F5");
-        PlayGame();
-
+        }
+        
+    scoreH.textContent = humanScore;
+    scoreC.textContent = PCScore;
     }
-
-}else{
-    alert("Game OVER");
-    if(humanScore > PCScore){
-        alert("Human wins");
-    }else if(humanScore < PCScore){
-        alert("PC wins");
-    }
-    alert("F5 to play again")
-}
 
 }
 
-
-PlayGame();
